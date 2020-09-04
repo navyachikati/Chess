540a11f08a684f18ab439ed236ab2fabd8869642
AnushaBoddapati <anuanusha419@gmail.com>
2020-09-04 19:10:58 +0530

    random commit

--- src/main/java/com/chessgame/Knights.java
+++ src/main/java/com/chessgame/Knights.java
@@ -1,4 +1,21 @@
 package com.chessgame;
 public class Knights extends Coin{
 
+	public Knights(boolean white, boolean alive) {
+		super(white, alive);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean move(Spot start, Spot end) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isRoutePossible(Spot start, Spot end) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
