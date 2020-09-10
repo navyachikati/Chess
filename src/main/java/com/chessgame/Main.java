ded0b7bcff229cf88568dc77849e5cd35fbe2237
AnushaBoddapati <anuanusha419@gmail.com>
2020-09-10 23:11:26 +0530

    10-9-2020

--- src/main/java/com/chessgame/Main.java
+++ src/main/java/com/chessgame/Main.java
@@ -3,7 +3,16 @@ package com.chessgame;
 public class Main {
 
 	public static void main(String[] args) {
-	
+		
+		Player player1 = new Player(true, "Sai");
+		Player player2 = new Player(false, "Navya");
+		Player[] players = new Player[2];
+		players[0] = player1;
+		players[1] = player2;
+		Game game = new Game(players);
+		// two platers will start playing one by one. 
+		
+		game.move(player1, 1, 1, 1, 2);
 	}
 
 }
